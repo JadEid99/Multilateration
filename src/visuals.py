23ac24686624d797cd26d59e2b2fde89b63c6f81
Jad Eid <jad.eid@sewts.de>
2022-07-18 21:09:09 +0200

    Added bg image to plot

--- src/visuals.py
+++ src/visuals.py
@@ -9,10 +9,16 @@ for position in initial_coordinates:
     team[position] = (Player(position))
     team[position].assign_coordinates()
 
-print(team["LW"].starting_coordinates)
 
 for player in team:
     coordinates = team[player].starting_coordinates
     plt.scatter(coordinates[0], coordinates[1], c="r", s=100, alpha=1)
     plt.annotate(player, (coordinates[0], coordinates[1]))
+
+plt.xlim([0, 60])
+plt.ylim([0, 100])
+plt.rcParams["figure.figsize"] = [7.00, 3.50]
+plt.rcParams["figure.autolayout"] = True
+im = plt.imread("assets/football.jpeg")
+plt.imshow(im, extent=[0, 60, 0, 100])
 plt.show()
