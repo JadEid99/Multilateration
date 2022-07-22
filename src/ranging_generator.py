a5b07e655cb2fec05a2b7235302a781d26b42a82
Jad Eid <jad.eid@sewts.de>
2022-07-23 00:17:24 +0200

    Added Kalman filter and documentation

--- src/ranging_generator.py
+++ src/ranging_generator.py
@@ -1,9 +1,12 @@
 from scipy.optimize import minimize
-from src.config_loader import ConfigLoader
 import numpy as np
 import random
 import math
 
+from src.config_loader import ConfigLoader
+
+
+# Configuration loading for initial player positions and simulation parameters from config files
 player_config = ConfigLoader("config/initial_coordinates.yaml")
 simulation_config = ConfigLoader("config/simulation_parameters.yaml")
 initial_coordinates = player_config.load_config()
@@ -11,17 +14,50 @@ simulation_parameters = simulation_config.load_config()
 
 
 class Player:
-    def __init__(self, position, current_coordinates=(0, 0), velocity_vector=[0, 0]):
+    """
+       A class to represent players on a football pitch.
+
+       ...
+
+       Attributes
+       ----------
+       position : str
+           player position in football terms
+       velocity vector : list of float
+           v_x and v_y of the player
+       current coordinates : tuple of float
+           exact coordinates of player from config file and player movement
+       estimated_coordinates: tuple of float
+           estimated coordinates of player from multilateration
+       """
+    def __init__(self, position, velocity_vector=[0, 0], current_coordinates=(0, 0), estimated_coordinates=(0, 0)):
+
         self.position = position
         self.current_coordinates = current_coordinates
         self.velocity_vector = velocity_vector
+        self.estimated_coordinates = estimated_coordinates
 
     def assign_coordinates(self):
+        """
+        Assigns player coordinates from config file.
+
+        Returns
+        -------
+        None
+        """
         self.current_coordinates = player_config.load_config()[self.position]
 
     def random_velocity(self):
+        """
+        Assigns random velocity vector for each player. PLayer movement direction is dependent on current player coordinates.
+
+        Returns
+        -------
+        None
+        """
         average_speed = random.randint(0, 8)  # average football player speed is around 8 m/s
 
+        # Method identifies the quadrant that the player is located in and only allows movement to keep player on the pitch (works to a certain extent).
         quadrant1 = simulation_parameters["origin"][0] <= self.current_coordinates[0] <= \
                     simulation_parameters["field_size"][0] / 2 \
                     and simulation_parameters["origin"][1] <= self.current_coordinates[1] <= \
@@ -52,16 +88,31 @@ class Player:
             movement_trajectory = random.randint(180, 270)
         else:
             average_speed = 0
+            movement_trajectory = 0
         self.velocity_vector = [
             float(average_speed * math.cos(movement_trajectory * math.pi / 180)),
             float(average_speed * math.sin(movement_trajectory * math.pi / 180))]
 
     def move_player(self):
+        """
+        Moves player using the velocity vector.
+
+        Returns
+        -------
+        None
+        """
         move_vector = [n * 1 / simulation_parameters["sensor_frequency"] for n in self.velocity_vector]
         self.current_coordinates = [self.current_coordinates[0] + move_vector[0],
                                     self.current_coordinates[1] + move_vector[1]]
 
     def receiver_distance(self):
+        """
+        Calcualtes the distance between the player (sensor) and the receivers in each of the corners of the pitch.
+
+        Returns
+        -------
+        sensor_array: list of floats
+        """
         bottom_right_sensor = math.hypot(self.current_coordinates[0] - simulation_parameters["bottom_right_corner"][0],
                                          self.current_coordinates[1] - simulation_parameters["bottom_right_corner"][1])
         bottom_left_sensor = math.hypot(self.current_coordinates[0] - simulation_parameters["origin"][0],
@@ -75,9 +126,24 @@ class Player:
 
     @staticmethod
     def noise_generator(sensor_array):
+        """
+        Applies noise to distance values calculated in previous method.
+
+        Returns
+        -------
+        sensor_array: list of floats
+        """
         return sensor_array + np.random.normal(-0.3, 0.3, 1)
 
     def multilateration(self):
+        """
+        Implements the multilateration algorithm that, given the coordinates of the receivers on the corners of the pitch,
+        and given their distances to the sensor/player, computes the most probable coordinates of the sensor/player.
+
+        Returns
+        -------
+        None
+        """
         def error(x, c, r):
             return sum([(np.linalg.norm(x - c[i]) - r[i]) ** 2 for i in range(len(c))])
 
@@ -94,7 +160,80 @@ class Player:
         # get initial guess of point location
         x0 = sum([W[i] * receiver_coordinates[i] for i in range(l)])
         # optimize distance from signal origin to border of spheres
-        return minimize(error, x0, args=(receiver_coordinates, distances_to_receiver), method='Nelder-Mead').x
+        self.estimated_coordinates = minimize(error, x0, args=(receiver_coordinates, distances_to_receiver), method='Nelder-Mead').x
 
-    #def handle_noise(self):
 
+class KalmanFilter():
+    """
+       A class for the KalmanFilter implementation.
+
+       ...
+
+       Attributes
+       ----------
+       time_interval : list of int
+           simulation time
+       estiamted_x_coordinates : list of float
+           x coordinates returned from multilateration
+       estimated_y_coordinates : list of float
+           y coordinates returned from multilateration
+       F : numpy array
+           the state-transition model
+       B : numpy array
+           is the control-input model which is applied to the control vector
+       H : numpy array
+           the observation model
+       Q : numpy array
+           the covariance of the process noise
+       R : numpy array
+           the covariance of the observation noise
+       P : numpy array
+       x0 : numpy array
+       """
+    def __init__(self, time_interval, estimated_x_coordinates, estimated_y_coordinates, F = None, B = None, H = None, Q = None, R = None, P = None, x0 = None):
+
+        if(F is None or H is None):
+            raise ValueError("Set proper system dynamics.")
+
+        self.n = F.shape[1]
+        self.m = H.shape[1]
+
+        self.time_interval = time_interval
+        self.estimated_x_coordinates = estimated_x_coordinates
+        self.estimated_y_coordinates = estimated_y_coordinates
+        self.F = F
+        self.H = H
+        self.B = 0 if B is None else B
+        self.Q = np.eye(self.n) if Q is None else Q
+        self.R = np.eye(self.n) if R is None else R
+        self.P = np.eye(self.n) if P is None else P
+        self.x = np.zeros((self.n, 1)) if x0 is None else x0
+
+    def predict(self, u = 0):
+        self.x = np.dot(self.F, self.x) + np.dot(self.B, u)
+        self.P = np.dot(np.dot(self.F, self.P), self.F.T) + self.Q
+        return self.x
+
+    def update(self, z):
+        y = z - np.dot(self.H, self.x)
+        S = self.R + np.dot(self.H, np.dot(self.P, self.H.T))
+        K = np.dot(np.dot(self.P, self.H.T), np.linalg.inv(S))
+        self.x = self.x + np.dot(K, y)
+        I = np.eye(self.n)
+        self.P = np.dot(np.dot(I - np.dot(K, self.H), self.P), (I - np.dot(K, self.H)).T) + np.dot(np.dot(K, self.R), K.T)
+
+    def filter(self):
+        measurements_x = self.estimated_x_coordinates
+        measurements_y = self.estimated_y_coordinates
+
+        predictions_x = []
+        predictions_y = []
+
+        for z in measurements_x:
+            predictions_x.append(np.dot(self.H,  self.predict())[0])
+            self.update(z)
+
+        for z in measurements_y:
+            predictions_y.append(np.dot(self.H,  self.predict())[0])
+            self.update(z)
+        return predictions_x, predictions_y
